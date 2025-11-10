86ee52f0896c2622437adbc258fd2bd70d78af72
Sibonsio <siboniso1sekute@gmail.com>
2025-11-10 13:01:01 +0200

    book keeping model

---
+++ src/models/booKeepingModel.js
@@ -0,0 +1,15 @@
+import  mongoose from 'mongoose'
+
+const booKeepingSchema = mongoose.Schema({
+    firstname:{type:String,
+    required:[true, 'Firstname is required']},
+    lastname:{type:String,
+        required:[true, 'Lastname is required']},
+    age:{type:Number,
+    required:[true, 'Age is required']},
+    employmentStatus:{type:String,
+        required:[true, 'Employment Status is required'], enum:[employed, unemployed]}, 
+}, {timestamps: true, versionKey: false})
+
+const booKeepingModel = mongoose.model('booKeepingModel', booKeepingSchema)
+export default booKeepingModel

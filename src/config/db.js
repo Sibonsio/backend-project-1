b7853c8967ff9c0587af63f21aeeda5c6f664d68
Sibonsio <siboniso1sekute@gmail.com>
2025-11-10 12:22:31 +0200

    Server Running & Database connected

---
+++ src/config/db.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const connectDB = async ()=>{
+   await mongoose.connect(process.env.MANGODB_URI).then(()=>{
+    console.log('Database is connected')
+})
+.catch((error)=>{
+    console.log(`MANGODB Connection Error: ${error.message}`)
+})};
+export default connectDB;
